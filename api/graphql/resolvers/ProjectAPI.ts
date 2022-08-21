03aeb833c506390f69d2718d0295f0765218bedb
strandgeek <strandgeek@gmail.com>
2022-08-21 19:14:01 -0300

    feat(backend): implement create minted tokens endpoint for SDK

--- api/graphql/resolvers/ProjectAPI.ts
+++ api/graphql/resolvers/ProjectAPI.ts
@@ -1,9 +1,25 @@
 import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
-import { ProjectAsset } from '@generated/type-graphql'
+import { ProjectAsset, MintedProjectAsset } from '@generated/type-graphql'
 import { Context } from "../../types/context";
+
+@InputType()
+export class MintedProjectAssetCreateInput {
+  @Field()
+  slug: string;
+
+  @Field()
+  assetID: number;
+
+  @Field()
+  txnID: string;
+
+  @Field()
+  to: string;
+}
 
 @Resolver()
 export class ProjectAPIResolver {
+
   @Query(() => ProjectAsset)
   async assetInfoByApiToken(
     @Arg("apiToken") apiToken: string,
@@ -30,5 +46,75 @@ export class ProjectAPIResolver {
     })
 
     return projectAsset
+  }
+
+  @Query(() => MintedProjectAsset)
+  async mintedAssetInfoByApiToken(
+    @Arg("apiToken") apiToken: string,
+    @Arg("assetID") assetID: number,
+    @Ctx() ctx: Context
+  ): Promise<MintedProjectAsset | null> {
+    const { prisma } = ctx
+
+    const project = await prisma.project.findUnique({
+      where: {
+        apiToken,
+      }
+    })
+
+    if (!project) {
+      throw new Error('Forbidden')
+    }
+
+    const mintedProjectAsset = await prisma.mintedProjectAsset.findFirstOrThrow({
+      where: {
+        projectId: project.id,
+        assetID,
+      }
+    })
+
+    return mintedProjectAsset
+  }
+
+  @Mutation(() => MintedProjectAsset)
+  async createMintedProjectAssetByApiToken(
+    @Arg("input") input: MintedProjectAssetCreateInput,
+    @Arg("apiToken") apiToken: string,
+    @Ctx() ctx: Context
+  ): Promise<MintedProjectAsset | null> {
+    const { prisma } = ctx
+
+    const project = await prisma.project.findUnique({
+      where: {
+        apiToken,
+      }
+    })
+
+    if (!project) {
+      throw new Error('Forbidden')
+    }
+
+    const projectAsset = await prisma.projectAsset.findFirstOrThrow({
+      where: {
+        projectId: project.id,
+        slug: input.slug,
+      }
+    })
+
+    if (!project) {
+      throw new Error('Not Found')
+    }
+
+    const mintedProjectAsset = await prisma.mintedProjectAsset.create({
+      data: {
+        assetID: input.assetID,
+        txnID: input.txnID,
+        to: input.to,
+        projectId: project.id,
+        projectAssetId: projectAsset.id,
+      }
+    })
+
+    return mintedProjectAsset
   }
 }
